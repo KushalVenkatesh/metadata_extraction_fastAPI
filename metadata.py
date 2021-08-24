5d348f90d37370a21e12d0eb36bcb8ce82be7d2b
vandungdo <van-dung.do@bam.de>
2021-08-24 23:26:45 +0200

    add file type

--- metadata.py
+++ metadata.py
@@ -1,5 +1,6 @@
 import re
 import json
+import os
 
 # the function read each line and return metadata as key and value
 def get_metadata_in_one_line(line):
@@ -8,8 +9,31 @@ def get_metadata_in_one_line(line):
     result = s.split('\t')[:-1]
     return result
 
+# get file extention and base folder of the file
+def get_file_information(path, file):
+    completedFileName, fileExtention = os.path.splitext(os.path.join(path,file))
+    folderName = os.path.basename(path)
+    return fileExtention, folderName
+
 def eModul_metadata(filePath, fileName ):
     data = open(r"{}\{}".format(filePath,fileName),'r')
+    # get type of the file and name of base folder
+    fileExtention, experimentName = get_file_information(filePath, fileName)
+
+    if fileExtention == '.dat':
+        fileExtention = 'DATFILE'
+    elif fileExtention == '.csv':
+        fileExtention = 'CSVFile'
+    elif fileExtention == '.cad':
+        fileExtention = 'CADFILE'
+    else:
+        fileExtention = 'DocumentFile'
+
+    dataType = {
+        'data type': fileExtention
+    }
+
+    # read data file line by line
     lines = data.readlines()
     # get empty lines (where start and end the header)
     emptyLineIndex = []
@@ -49,7 +73,7 @@ def eModul_metadata(filePath, fileName ):
     }
 
     # aggregate the metadata
-    metadata = [serviceInformationDict,dataCollectionInformationDict,columnsDict]
+    metadata = [dataType, serviceInformationDict,dataCollectionInformationDict,columnsDict]
     # jsonData = json.dumps(serviceInformationDict,ensure_ascii=False)
     
     return metadata
